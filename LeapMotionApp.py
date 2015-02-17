74f68ef33a64864cf0c05c4a1c3ba90ae252dd58
Kimia Hassanzadeh <kim.hszd@gmail.com>
2015-02-16 17:07:28 -0700

    Frame/Hand/Arm/Fingers/Bones Data

--- LeapMotionApp.py
+++ LeapMotionApp.py
@@ -26,6 +26,7 @@ class LeapMotionListener(Leap.Listener):
 	def on_frame(self, controller):
 		frame = controller.frame()
 
+		
 		print "Frame ID: " + str(frame.id) \
 		+ " timeStamp: " + str(frame.timestamp) \
 		+ " #of Hands: " + str(len(frame.hands)) \
@@ -34,6 +35,7 @@ class LeapMotionListener(Leap.Listener):
 		+ " #of gesters: "+ str(len(frame.gestures()))
 
 		for hand in frame.hands:
+			
 			handType = "Left Hand" if hand.is_left else "Right Hand"
 
 			print handType + " Hand ID: "+str(frame.id) +" Palm Position: "+str(hand.palm_position)
@@ -44,6 +46,29 @@ class LeapMotionListener(Leap.Listener):
 			print " Pitch: "+ str(direction.pitch * Leap.RAD_TO_DEG) + \
 			      " Roll: " + str(normal.roll * Leap.RAD_TO_DEG) +\
 			      " Yaw: " + str(direction.yaw * Leap.RAD_TO_DEG)
+
+			print "----------"
+			arm = hand.arm
+			print "Arm Direction: "+ str(arm.direction) +" Wrist Position: "+ str(arm.wrist_position)+ " Elbow Position: "+ str(arm.elbow_position)
+			
+			
+			for finger in hand.fingers:
+				print " Finger Type: " + self.finger_names[finger.type()] + \
+					  " ID: "+ str(finger.id) +\
+					  " Length: "+ str(finger.length) + \
+					  " Width: "+ str(finger.width)
+
+				for b in range(0,4):
+					bone = finger.bone(b)
+					print "Bone: "+ self.bone_names[bone.type]+ \
+						  " Start: "+ str(bone.prev_joint)+ \
+						  " End: " + str(bone.next_joint) + \
+						  " Direction: "+ str(bone.direction)
+
+			for tool in frame.tools:
+				print "ID: "+ str(tool.id) + \
+					  " Tip Position: "+ str(tool.tip_position) + \
+					  " Direction: "+ str(tool.direction)
 
 
 def main():
